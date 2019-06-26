27827a8367744d01f810808fff53df356e350a09
johnzhu12 <1042532367@qq.com>
2019-06-26 17:25:40 +0800

    add readme

--- SICP练习题/chapter1/1.11.js
+++ SICP练习题/chapter1/1.11.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ *  n<3 f(n) = n
+ *  n>=3 f(n) = f(n-1)+ 2f(n-2)+3f(n-3)
+ */
 function cal(n) {
     return calInter(0, 1, 2, n)
 }
@@ -12,3 +17,16 @@ function calInter(a, b, c, count) {
 
 var re = cal(4);
 console.log(re)
+
+/*每一次迭代
+
+c  |  b   |  a
+2  |  1   |  0     first round
+sum|  2(c)|  1(b)  second
+
+所以就是
+  c-->  b
+  b---> a
+  sum -- >c
+
+*/
